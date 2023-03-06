2a43843aa568d4fce77ae2bf94f86c6b86f5772c
alex_man <alexmnsbs@gmail.com>
2023-03-06 19:23:41 +0300

    is first_

---
+++ start-service-with-docker-compose/first-dc/app/my-app.py
@@ -0,0 +1,8 @@
+import pymongo import MongoClient
+import pprint import pprint
+
+MONGO_URL = "mongodb://mongo:27017"
+client = MongoClient(MONGO_URL)
+db = client.admin
+dbs_list = db.command("listDatabases")
+pprint(dbs_list)
